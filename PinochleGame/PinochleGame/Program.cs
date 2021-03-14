b03089091fb7d92c305b922af15f53d8633ecb85
Daniel Wathen <wathen.daniel@gmail.com>
2021-03-14 14:06:54 -0700

    Files Added

---
+++ PinochleGame/PinochleGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PinochleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is Pinochle.");
+        }
+    }
+}
